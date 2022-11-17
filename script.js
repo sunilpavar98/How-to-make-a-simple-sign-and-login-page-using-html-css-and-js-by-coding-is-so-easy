ec66eb89bd5077e9c5e1fc939837767d3bd8fdca
Sunil Pavar <55226366+sunilpavar98@users.noreply.github.com>
2022-11-17 17:03:01 +0530

    I Make Login And SignUp Page Using html css and js
    I Make Login And SignUp Page Using html css and js
    
    Full tutorials Goto Channal
    &#8595;
    Youtube channal : https://www.youtube.com/@codingissoeasy

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginText = document.querySelector(".title-text .login")
+const loginForm = document.querySelector("form.login") ;
+const loginBtn = document.querySelector("label.login") ;
+const signupBtn = document.querySelector("label.signup") ;
+const signupLink = document.querySelector("form .signup-link a") ;
+
