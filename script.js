87a01b61f1915d8ea04f8f26559ba08e198f0b73
Sunil Pavar <55226366+sunilpavar98@users.noreply.github.com>
2022-11-17 17:06:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const loginForm = document.querySelector("form.login") ;
 const loginBtn = document.querySelector("label.login") ;
 const signupBtn = document.querySelector("label.signup") ;
 const signupLink = document.querySelector("form .signup-link a") ;
+
